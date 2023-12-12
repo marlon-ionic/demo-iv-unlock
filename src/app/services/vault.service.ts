80a77513e3ee976f9c35c3f282b4503a3fd23148
Marlon Harrison <marlon@ionic.io>
2023-12-12 10:19:55 -0600

    Added default 'true' for vault lock state, comments to VaultService for clarity

--- src/app/services/vault.service.ts
+++ src/app/services/vault.service.ts
@@ -1,33 +1,48 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-import { BrowserVault, DeviceSecurityType, IdentityVaultConfig, Vault, VaultType } from '@ionic-enterprise/identity-vault';
+import { BrowserVault, Device, DeviceSecurityType, IdentityVaultConfig, Vault, VaultType } from '@ionic-enterprise/identity-vault';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VaultService {
-  private isLockedSubject = new BehaviorSubject<boolean>(false);
-  isLocked$ = this.isLockedSubject.asObservable();
+  private isLockedSubject = new BehaviorSubject<boolean>(true);
   private vault?: Vault | BrowserVault;
+
+  // Observable version of the isLocked property that can be used to react to vault lock/unlock events.
+  isLocked$ = this.isLockedSubject.asObservable();
+
   constructor(private readonly zone: NgZone) { }
 
-
+  // Initialize the vault. This method is called via an APP_INITIALIZER in app.module.ts
   async init() {
+
+    // Determine the vault type based on the platform. Use the DeviceSecurityType.Biometrics for native platforms
     const isNativePlatform = Capacitor.isNativePlatform();
+    const canUseBiometrics = await Device.isBiometricsEnabled();
     const config: IdentityVaultConfig = {
       key: 'io.ionic.demo-iv',
-      type: isNativePlatform ? VaultType.DeviceSecurity : VaultType.SecureStorage,
-      deviceSecurityType: isNativePlatform ? DeviceSecurityType.Biometrics: DeviceSecurityType.None,
+      type: canUseBiometrics ? VaultType.DeviceSecurity : VaultType.SecureStorage,
+      deviceSecurityType: canUseBiometrics ? DeviceSecurityType.Biometrics: DeviceSecurityType.None,
       shouldClearVaultAfterTooManyFailedAttempts: false,
       customPasscodeInvalidUnlockAttempts: 2,
       unlockVaultOnLoad: false,
     };
 
     // Per https://ionic.io/docs/identity-vault/troubleshooting#use-initialize (initialize() available since 5.11.0)
+    // Create the vault instance with an empty config. Use the initialize() method to configure the vault.
+    // This ensure that the native platform's is pro.
     this.vault = isNativePlatform ? new Vault() : new BrowserVault();
+
+    // Initialize the vault via the new initialize() method
     await this.vault?.initialize(config);
 
+    //Initialize the vault's lock state
+    const isLocked = await this.vault?.isLocked();
+    this.isLockedSubject.next(isLocked);
+
+    // Subscribe to the vault's lock/unlock events
     this.vault?.onLock(() => {
       console.log('Vault locked');
       this.zone.run(() => {
@@ -43,6 +58,8 @@ export class VaultService {
     this.vault?.onError((e) => {
       console.warn('Vault error', e);
     });
+
+    console.log('Vault initialized', 'isLocked', isLocked);
   }
 
   async isLocked(): Promise<boolean> {
